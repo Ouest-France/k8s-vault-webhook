3e6d2810a864d7966ecbb2a42a63d1ca712490ea
Pablo RUTH <contact@pablo-ruth.fr>
2019-10-16 17:41:14 +0200

    add webhook started message

--- api/server.go
+++ api/server.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/Ouest-France/k8s-vault-webhook/vault"
@@ -23,6 +24,7 @@ func (s *Server) Serve() error {
 
 	router.Post("/secret", s.secretHandler)
 
+	log.Printf("Webhook started, listening on %s", s.Listen)
 	err := http.ListenAndServeTLS(s.Listen, s.Cert, s.Key, router)
 	if err != nil {
 		return fmt.Errorf("failed to start http server: %s", err)
