713c02c49602756f6a08a86ad372450232d623bc
Pablo RUTH <contact@pablo-ruth.fr>
2019-10-25 10:27:50 +0200

    add a Router func to Server struct

--- api/server.go
+++ api/server.go
@@ -19,16 +19,21 @@ type Server struct {
 }
 
 func (s *Server) Serve() error {
-	router := chi.NewRouter()
-
-	router.Post("/secret", s.secretHandler)
-	router.Get("/status", s.statusHandler)
 
 	s.Logger.Infof("webhook started, listening on %s", s.Listen)
-	err := http.ListenAndServeTLS(s.Listen, s.Cert, s.Key, router)
+	err := http.ListenAndServeTLS(s.Listen, s.Cert, s.Key, s.Router())
 	if err != nil {
 		return fmt.Errorf("failed to start http server: %s", err)
 	}
 
 	return nil
+}
+
+func (s *Server) Router() *chi.Mux {
+	router := chi.NewRouter()
+
+	router.Post("/secret", s.secretHandler)
+	router.Get("/status", s.statusHandler)
+
+	return router
 }
